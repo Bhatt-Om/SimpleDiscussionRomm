c278dd49679844e4bc21efaa81b09a92f2b4d311
Bhatt-Om <122429893+Bhatt-Om@users.noreply.github.com>
2023-03-06 10:31:28 +0530

    Generate Views and create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'rooms/index'
-  get 'rooms/show'
-  get 'rooms/new'
-  get 'rooms/create'
+  root 'rooms#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :rooms
 end
